0c34f090c28f536d9a5f30952d9c4a08f48baa31
Alex Powell <alex@blindfrogindustries.com>
2014-09-01 14:29:13 +1000

    heaps tighter

--- wp-content/themes/marketing/footer.php
+++ wp-content/themes/marketing/footer.php
@@ -1,9 +1,15 @@
-<footer>
-	<div class="watermark">
-		<p>Site by: </p>
-		<a target=_blank href="http://www.newgenstudio.com">Newgen Studio</a>
-		<p>&copy; <?php bloginfo( 'name' ); ?> <?php echo date('Y'); ?> </p>
+<footer class="container">
+<div class="row">
+	<?php /*
+<div class="col-sm-6">
+		<p class="text-muted"><small>Site by <a target='_blank' href="http://www.newgenstudio.com">Newgen Studio</a></small></p>
 	</div>
+*/ ?>
+	<div class="col-sm-12">
+		<p class="text-muted text-center"><small>&copy; Copyright <?php echo date('Y'); ?> &mdash; Nebula Digital</small></p>
+	</div>
+</div>
+
 </footer>
 
 	<?php wp_footer(); ?>
