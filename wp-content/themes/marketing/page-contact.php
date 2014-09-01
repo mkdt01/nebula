7fb06524fd0e4b93dd09af0f3146fdc1a0203c74
Alex Powell <alex@blindfrogindustries.com>
2014-09-01 14:32:25 +1000

    consistency

--- wp-content/themes/marketing/page-contact.php
+++ wp-content/themes/marketing/page-contact.php
@@ -15,19 +15,19 @@ get_header(); ?>
 <div class="container">
 
 	<div class="row">
-		<div class="col-sm-8">
+		<div class="col-sm-7">
 			<h2> Contact Form </h2>
 			
 			<?php echo do_shortcode('[contact-form-7 id="300" title="Untitled"]'); ?>
 		
 		</div>
 		
-		<div class="col-sm-4 contact-details">
+		<div class="col-sm-5 contact-details">
 			<h2>Contact Nebula</h2>
 			<p>Feel Free to drop us an email on your next project to see how we can help you make it a success.</p>
 			<p class="address"><span class="glyphicon glyphicon-home"></span>985A Glen Huntly Road<br>Caulfield South<br>VIC 3162</p>
-			<p class="phone"><span class="glyphicon glyphicon-phone-alt"></span>+61 3 9020 2925</p>
-			<p class="info"><span class="glyphicon glyphicon glyphicon-envelope"></span>info@nebula.com</p>
+			<p class="phone"><span class="glyphicon glyphicon-phone-alt"></span><a href="tel:+61490202925">+61 3 9020 2925</a></p>
+			<p class="info"><span class="glyphicon glyphicon glyphicon-envelope"></span><a href="mailto:hello@nebuladigital.com.au" target="_blank">hello@nebuladigital.com.au</a></p>
 			
 		</div>
 	</div>
